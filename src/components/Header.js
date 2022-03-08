bbd7199c783b5ad1ad6871697976816fc3239199
Mohamed Noufal <getmohamednoufal@gmail.com>
2022-03-08 08:06:40 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,42 @@
+import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import HomeIcon from "@mui/icons-material/Home";
+import HeaderOption from "./HeaderOption";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsActiveIcon from "@mui/icons-material/NotificationsActive";
+import "../styles/Header.css";
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__left">
+                <img
+                    src="https://cdn-icons.flaticon.com/png/512/3536/premium/3536505.png?token=exp=1646647836~hmac=c6b9dbaf82821e38163917bd4147ad3c"
+                    alt="logo"
+                />
+                <div className="header__search">
+                    {/* search icon */}
+                    <SearchIcon />
+                    <input type="text" />
+                </div>
+            </div>
+            <div className="header__right">
+                <HeaderOption Icon={HomeIcon} title={"Home"} />
+                <HeaderOption Icon={SupervisorAccountIcon} title={"My Network"} />
+                <HeaderOption Icon={BusinessCenterIcon} title={"Jobs"} />
+                <HeaderOption Icon={ChatIcon} title={"Messaging"} />
+                <HeaderOption Icon={NotificationsActiveIcon} title={"Notifications"} />
+                <HeaderOption
+                    avatar={
+                        "https://media-exp1.licdn.com/dms/image/C5103AQF96ES8oDkyUA/profile-displayphoto-shrink_200_200/0/1555241079252?e=1652313600&v=beta&t=bYvc4hdCmqx7xIcDKbR689WahkU0rWve4gnZQeJyCBA"
+                    }
+                    title={"Me"}
+                />
+            </div>
+        </div>
+    );
+}
+
+export default Header;
